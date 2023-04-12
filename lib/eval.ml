8a6f1889d9bb4fd057c3009a4e7c52818122d67f
tarakojo1019 <tarakojo1019@gmail.com>
2023-04-12 22:09:55 +0900

    updated

--- lib/eval.ml
+++ lib/eval.ml
@@ -2,23 +2,30 @@ open Syntax
 
 exception Eval_error 
 
+
+let printEnv env = 
+  print_string "[";
+  List.iter (fun (x, v) -> print_string "(" ; print_string x; print_string "," ; Print.printValue v; print_string ")") env;
+  print_string "]"
+
+
 let rec eval env e = 
   let getint v = 
     match v with
     | VInt x -> x 
-    | _ -> raise Eval_error 
+    | _ -> raise Eval_error
   in 
   let getbool v = 
     match v with
     | VBool x -> x 
-    | _ -> raise Eval_error 
+    | _ -> raise Eval_error
   in 
   match e with
   | EValue v -> v 
   | EBin (op, e1, e2) -> 
-    let v1 = getint (eval env e1) in 
-    let v2 = getint (eval env e2) in (
-    match op with
+    let v1 = getint (eval env e1) in
+    let v2 = getint (eval env e2) in 
+    (match op with
     | OpAdd -> VInt (v1 + v2)
     | OpSub -> VInt (v1 - v2)
     | OpMul -> VInt (v1 * v2)
@@ -41,10 +48,11 @@ let rec eval env e =
       eval ((f, VRFun(f,x,e1,env)) :: env) e2
   | EAbs (x, e) -> VFun (x, e, env)
   | EApp (e1, e2) -> 
-      match (eval env e1, eval env e2) with
-      | (VFun (x, e, env'), v) -> 
-          eval ((x, v) :: env') e 
-      | (VRFun (f, x, e, env'), v) -> 
-          eval ((x, v) :: (f, VRFun(f,x,e1,env')) :: env') e
+      let fv = eval env e1 in 
+      let v = eval env e2 in 
+      match fv with
+      | VFun (x, e', env') -> 
+          eval ((x, v) :: env') e' 
+      | VRFun (f, x, e', env') -> 
+          eval ( (x, v) :: (f, VRFun(f,x,e',env')) :: env') e'
       | _ -> raise Eval_error  
-        
--- lib/print.ml
+++ lib/print.ml
@@ -6,3 +6,71 @@ let printValue = function
     | VBool b -> print_string (if b then "true" else "false")
     | VFun _ -> print_string "function"
     | VRFun _ -> print_string "rec function"
+
+
+let rec printExpr : expr -> unit = function 
+    | EValue v -> 
+        print_string "EValue("; 
+        (match v with
+        | VInt x -> print_string "VInt(" ; print_int x ; print_string "))"
+        | VBool x -> print_string ("VBool(" ^ (if x then "true" else "false")); print_string "))"
+        | VFun _ -> print_string "VFun"
+        | VRFun _ -> print_string "VRFun")
+    | EBin (op, e1, e2)  -> 
+        print_string "EBin(";
+        (match op with
+        | OpAdd -> print_string "+,"
+        | OpSub -> print_string "-,"
+        | OpMul -> print_string "*,"
+        | OpDiv -> print_string "/,");
+        printExpr e1; print_string ","; printExpr e2; print_string ")"
+    | EEq (e1, e2) -> 
+        print_string "EEq(";
+        printExpr e1 ; 
+        print_string ",";
+        printExpr e2; 
+        print_string ")"
+    | ELt (e1, e2) -> 
+        print_string "ELt(";
+        printExpr e1 ; 
+        print_string ",";
+        printExpr e2; 
+        print_string ")"
+    | EIf (cond, e1, e2) -> 
+        print_string "EIf(";
+        printExpr cond; 
+        print_string ",";
+        printExpr e1; 
+        print_string ",";
+        printExpr e2; 
+        print_string ")"
+    | EVar x -> 
+        print_string "EVar(";
+        print_string x ;
+        print_string ")"
+    
+    | ELet (x, e1, e2) -> 
+        print_string ("ELet("^x^",");
+        printExpr e1; 
+        print_string ",";
+        printExpr e2 ;
+        print_string ")"
+    
+    | ERLet (f, x, e1, e2) -> 
+        print_string ("ERLet("^f^","^x^",");
+        printExpr e1;
+        print_string ",";
+        printExpr e2 ; 
+        print_string ")"
+    
+    | EAbs (x, e) -> 
+        print_string ("EAbs("^x^",");
+        printExpr e ;
+        print_string ")"
+
+    | EApp (e1, e2) -> 
+        print_string ("EApp(");
+        printExpr e1; 
+        print_string ",";
+        printExpr e2;
+        print_string ")"
